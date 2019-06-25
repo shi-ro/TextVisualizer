4f66b7e3a572cd35b957dcc818df8fb18eeaad8e
Kuro <body.identity@outlook.com>
2019-06-25 20:43:27 +0200

    uploaded TextVisualizer

---
+++ TextToImage/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace TextToImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1260, 42);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip1_ItemClicked);
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(181, 38);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(181, 38);
+            this.saveToolStripMenuItem.Text = "Export";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 43);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.ForcedBoth;
+            this.richTextBox1.Size = new System.Drawing.Size(1236, 684);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 734);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(211, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(230, 734);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(204, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Compress";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(440, 738);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(1127, 734);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(121, 35);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Encode";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1260, 782);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TextToImage/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TextToImage
+{
+    public partial class Form1 : Form
+    {
+        public Bitmap TextImage;
+        private Color unknown = Color.White;
+        private string OkChars = @"!""#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+        private string BlueChars = @"!""#$%&'()*+,-./";
+        private string MagentaChars = @"0123456789:;<=>";
+        private string RedChars = @"?@ABCDEFGHIJKLMN";
+        private string YellowChars = @"OPQRSTUVWXYZ[\]^";
+        private string GreenChars = @"_`abcdefghijklmn";
+        private string CyanChars = @"opqrstuvwxyz{|}~";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text.Length < 1)
+            {
+                return;
+            }
+            string[] boxl = richTextBox1.Lines;
+            int height = boxl.Length;
+            int width = 0;
+            for (var i = 0; i < boxl.Length; i++)
+            {
+                if (boxl[i].Length > width)
+                {
+                    width = boxl[i].Length;
+                }
+            }
+            TextImage = new Bitmap(width,height);
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    TextImage.SetPixel(x,y,Color.Black);
+                }
+            }
+            for (var i = 0; i < boxl.Length; i++)
+            {
+                for (var i1 = 0; i1 < boxl[i].ToCharArray().Length; i1++)
+                {
+                    char c = boxl[i].ToCharArray()[i1];
+                    if (OkChars.Contains(c))
+                    {
+                        Color pixelColor = new Color();
+                        //===========================
+                        
+                        //B->M:(0, 0, 255)
+                        //    +..
+                        if (BlueChars.Contains(c))
+                        {
+                            double hoff = 255 / BlueChars.Length;
+                            double intensity = BlueChars.IndexOf(c) * hoff;
+                            pixelColor = Color.FromArgb((int) intensity, 0, 255);
+                        }
+                        //M->R:(255, 0, 255)
+                        //    ..-
+                        if (MagentaChars.Contains(c))
+                        {
+                            double hoff = 255 / MagentaChars.Length;
+                            double intensity = MagentaChars.IndexOf(c) * hoff;
+                            pixelColor = Color.FromArgb(255, 0, 255 - (int)intensity);
+                        }
+                        //R->Y:(255, 0, 0)
+                        //    .+.
+                        if (RedChars.Contains(c))
+                        {
+                            double hoff = 255 / RedChars.Length;
+                            double intensity = RedChars.IndexOf(c) * hoff;
+                            pixelColor = Color.FromArgb(255, (int) intensity, 0);
+                        }
+                        //Y->G:(255, 255, 0)
+                        //    -..
+                        if (YellowChars.Contains(c))
+                        {
+                            double hoff = 255 / YellowChars.Length;
+                            double intensity = YellowChars.IndexOf(c) * hoff;
+                            pixelColor = Color.FromArgb(255 - (int) intensity, 255, 0);
+                        }
+                        //G->C:(0, 255, 0)
+                        //    ..+
+                        if (GreenChars.Contains(c))
+                        {
+                            double hoff = 255 / GreenChars.Length;
+                            double intensity = GreenChars.IndexOf(c) * hoff;
+                            pixelColor = Color.FromArgb(0, 255, (int) intensity);
+                        }
+                        //C->B:(0, 255, 255)
+                        //    .-.
+                        if (CyanChars.Contains(c))
+                        {
+                            double hoff = 255 / CyanChars.Length;
+                            double intensity = CyanChars.IndexOf(c) * hoff;
+                            pixelColor = Color.FromArgb(0, 255 - (int) intensity, 255);
+                        }
+                        //============================
+                        TextImage.SetPixel(i1,i,pixelColor);
+                    }
+                    else
+                    {
+                        if (c != ' ')
+                        {
+                            TextImage.SetPixel(i1, i, unknown);
+                        }
+                    }
+                }
+            }
+            //save bitmap
+            TextImage.Save(@"C:\Users\usagi\Desktop\image.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox1.Text;
+            int width = (int) numericUpDown1.Value;
+            int height = (int)Math.Ceiling((double) text.Length / width);
+            int x = 0;
+            int y = 0;
+            TextImage = new Bitmap(width, height);
+            for (int y1 = 0; y1 < height; y1++)
+            {
+                for (int x1 = 0; x1 < width; x1++)
+                {
+                    TextImage.SetPixel(x1,y1,Color.Black);
+                }
+            }
+            foreach (char c in text)
+            {
+                if (x >= width)
+                {
+                    x = 0;
+                    y++;
+                }
+                if (OkChars.Contains(c))
+                {
+                    Color pixelColor = new Color();
+                    //===========================
+
+                    //B->M:(0, 0, 255)
+                    //    +..
+                    if (BlueChars.Contains(c))
+                    {
+                        double hoff = 255 / BlueChars.Length;
+                        double intensity = BlueChars.IndexOf(c) * hoff;
+                        pixelColor = Color.FromArgb((int)intensity, 0, 255);
+                    }
+                    //M->R:(255, 0, 255)
+                    //    ..-
+                    if (MagentaChars.Contains(c))
+                    {
+                        double hoff = 255 / MagentaChars.Length;
+                        double intensity = MagentaChars.IndexOf(c) * hoff;
+                        pixelColor = Color.FromArgb(255, 0, 255 - (int)intensity);
+                    }
+                    //R->Y:(255, 0, 0)
+                    //    .+.
+                    if (RedChars.Contains(c))
+                    {
+                        double hoff = 255 / RedChars.Length;
+                        double intensity = RedChars.IndexOf(c) * hoff;
+                        pixelColor = Color.FromArgb(255, (int)intensity, 0);
+                    }
+                    //Y->G:(255, 255, 0)
+                    //    -..
+                    if (YellowChars.Contains(c))
+                    {
+                        double hoff = 255 / YellowChars.Length;
+                        double intensity = YellowChars.IndexOf(c) * hoff;
+                        pixelColor = Color.FromArgb(255 - (int)intensity, 255, 0);
+                    }
+                    //G->C:(0, 255, 0)
+                    //    ..+
+                    if (GreenChars.Contains(c))
+                    {
+                        double hoff = 255 / GreenChars.Length;
+                        double intensity = GreenChars.IndexOf(c) * hoff;
+                        pixelColor = Color.FromArgb(0, 255, (int)intensity);
+                    }
+                    //C->B:(0, 255, 255)
+                    //    .-.
+                    if (CyanChars.Contains(c))
+                    {
+                        double hoff = 255 / CyanChars.Length;
+                        double intensity = CyanChars.IndexOf(c) * hoff;
+                        pixelColor = Color.FromArgb(0, 255 - (int)intensity, 255);
+                    }
+                    //============================
+                    TextImage.SetPixel(x, y, pixelColor);
+                    x++;
+                }
+                else
+                {
+                    if (c != ' ')
+                    {
+                        TextImage.SetPixel(x, y, unknown);
+                        x++;
+                    }
+                }
+            }
+            TextImage.Save(@"C:\Users\usagi\Desktop\image.png", System.Drawing.Imaging.ImageFormat.Png);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string[] lines = richTextBox1.Lines;
+            List<int[]> it = new List<int[]>();
+            foreach (var l in lines)
+            {
+                char[] c = l.ToCharArray();
+                int[] t = new int[c.Length];
+                for (var i = 0; i < c.Length; i++)
+                {
+                    t[i] = c[i];
+                }
+                it.Add(t);
+            }
+            richTextBox1.Text = "";
+            foreach (var its in it)
+            {
+                foreach (var i in its)
+                {
+                    char[] chr = ("" + i).ToCharArray();
+                    string total = "";
+                    for (var i1 = 0; i1 < chr.Length; i1++)
+                    {
+                        string cur = "";
+                        char c = chr[i1];
+                        if (c == '0')
+                        {
+                            cur = "z#Ro`";
+                        }
+                        if (c == '1')
+                        {
+                            cur = "%iNg`";
+                        }
+                        if (c == '2')
+                        {
+                            cur = "tW!n`";
+                        }
+                        if (c == '3')
+                        {
+                            cur = "t#rE`";
+                        }
+                        if (c == '4')
+                        {
+                            cur = "SqU*`";
+                        }
+                        if (c == '5')
+                        {
+                            cur = "P3Nt`";
+                        }
+                        if (c == '6')
+                        {
+                            cur = "H#6a`";
+                        }
+                        if (c == '7')
+                        {
+                            cur = "W3Ek`";
+                        }
+                        if (c == '8')
+                        {
+                            cur = ")Ct0`";
+                        }
+                        if (c == '9')
+                        {
+                            cur = "N1n#`";
+                        }
+                        total += cur;
+                    }
+                    richTextBox1.Text += total+ @" ";
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
